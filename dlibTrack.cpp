49eebd57208d09695539183b70e1f213bbb091a1
Arjun <arjunchau@gmail.com>
2019-07-05 10:14:03 +0300

    Buggy MultiTracker

--- dlibTrack.cpp
+++ dlibTrack.cpp
@@ -3,17 +3,25 @@
 #include "opencv2/imgproc.hpp"
 #include <opencv2/tracking.hpp>
 #include <opencv2/core/ocl.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/videoio.hpp>
+#include <cstring>
+#include <ctime>
+#include "samples_utility.hpp"
 
 #include<iostream>
+
+#define RESET   "\033[0m"
+#define RED     "\033[1;31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW "\033[33m"  /* Yellow */
 
 using namespace std;
 using namespace cv;
 
-void detect(Mat frame);
-
-Rect2d bbox;
-
 CascadeClassifier car_cascade;
+
+//void resetTracker(*cv::MultiTracker);
 
 int main( int argc, const char **argv)
 {
@@ -57,8 +65,11 @@ int main( int argc, const char **argv)
     Mat cropped;
 
     //create tracker pointer
-    Ptr<Tracker> tracker;
-    tracker = TrackerKCF::create();
+
+    String trackingAlg = "KCF";
+    MultiTracker trackers;
+    vector<Rect2d> objects;
+    std::vector<Ptr<Tracker> > algorithms;
 
     int frameCount = 0;
 
@@ -85,31 +96,41 @@ int main( int argc, const char **argv)
             cout <<"[ERROR] No Frame Captured"<<endl;
         }
 
-        if(frameCount%20 == 0) //check if 10 frames have passed
+
+        if (frameCount%50 == 0)
+        {
+            //resetTracker(*trackers);
+            MultiTracker *trackers;
+            cout <<"Reset"<<endl;
+        }
+
+
+        if(frameCount%40 == 0) //check if 10 frames have passed
         {
             //cout <<"[INFO] 20 done"<<endl;
             Mat gray;
             Mat cropped;
             Mat mask = Mat::zeros(cvSize(cols, rows),CV_8UC1);
 
+            objects.clear(); //let go of old tracks
+            algorithms.clear();
+            cout <<"Trackers "<<trackers.getObjects().size()<<endl;
+            
+
             cvtColor(frame, gray, COLOR_BGR2GRAY);
             equalizeHist(gray, gray);
 
             fillPoly( mask,ppt, &npt,1,Scalar( 255, 255, 255 ));
-
-            
-
             bitwise_and(gray, mask, cropped);
 
             imshow("Cropped", cropped);
+            
             std::vector<Rect> cars;
             car_cascade.detectMultiScale(cropped, cars, 
                                     1.4,
                                     3,
                                     0,
                                     cvSize(50,50));
-
-
             for (size_t i = 0 ; i < cars.size(); i ++)
             {
                 Point center(cars[i].x+cars[i].width/2, cars[i].y+cars[i].height/2);
@@ -120,22 +141,37 @@ int main( int argc, const char **argv)
                 circle(frame, center, 1, Scalar(255,0,0), 2);
                 rectangle(frame, vertex1, vertex2, Scalar(0,0,255),2);
 
-                bbox = Rect2d(cars[0].x, cars[0].y , cars[0].x+cars[0].width, cars[0].y+cars[0].height);
+                //create a bounding box and add it to the tracker              
+                algorithms.push_back(createTrackerByName(trackingAlg));
+                objects.push_back(cars[i]);
+                trackers.add(algorithms,cropped,objects);
 
                 //Check for hazard
                 if (center.y < rows-70 && center.y > rows - 200)
                 {
                     if (cars[i].y+cars[i].height > rows-70)
                     {
-                        cout <<"[ALERT!] STOP!"<<endl;
+                        cout <<RED "[ALERT!] STOP!" RESET<<endl;
                     }
 
-                    cout <<"[WARNING] VEHICLE APPROACHING"<<endl;
-                }
-
-                
+                    cout <<YELLOW "[WARNING] VEHICLE APPROACHING" RESET<<endl;
+                }              
             }
         }
+
+        
+        bool ok = trackers.update(frame);
+        //cout <<trackers.getObjects().size()<< endl;
+        for(unsigned i=0;i<trackers.getObjects().size();i++)
+                rectangle( frame, trackers.getObjects()[i], Scalar( 255, 255, 255 ), .5, 1 );
+
+        /*if (ok)
+        {   
+            cout <<"IN"<<endl;
+
+            for(unsigned i=0;i<trackers.getObjects().size();i++)
+                rectangle( frame, trackers.getObjects()[i], Scalar( 255, 255, 255 ), 2, 1 );
+        }*/
 
         /*bool ok = tracker->update(frame, bbox);
         if (ok)
@@ -151,9 +187,9 @@ int main( int argc, const char **argv)
         Point RedStart = Point(0, rows-70);
         Point RedEnd = Point(cols, rows-70);
 
-        line(frame, YellowStart, YellowEnd, Scalar(0,255,255), 1);
-        line(frame, RedStart, RedEnd, Scalar(0,0,255), 1);
-        polylines(frame, ppt, &npt, 1,true, Scalar(255,0,255), 0.5 );
+        //line(frame, YellowStart, YellowEnd, Scalar(0,255,255), 1);
+        //line(frame, RedStart, RedEnd, Scalar(0,0,255), 1);
+        //polylines(frame, ppt, &npt, 1,true, Scalar(255,0,255), 0.5 );
         imshow("Detected", frame);
 
         frameCount++;
